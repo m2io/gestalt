1cfe6c1e92df03123ac0c735e039791585dae503
m2io <grmnn@pm.me>
2024-10-06 22:50:19 +0200

    feat(tauri): configure first migration with weights table and preload db

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,34 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "
+						CREATE TABLE IF NOT EXISTS weights (
+								id      INTEGER PRIMARY KEY AUTOINCREMENT,
+								date    TEXT NOT NULL,
+								weight  REAL NOT NULL
+						);
+				",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "delete_initial_tables",
+            sql: "DROP TABLE IF EXISTS weights;",
+            kind: MigrationKind::Down,
+        },
+    ];
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:gestalt.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
