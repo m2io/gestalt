8b6d78831105a048d776b453e0cbd5321518c90e
m2io <grmnn@pm.me>
2024-10-07 21:14:22 +0200

    feat(app): enable auto generated storybook docs and adapt light/dark mode colors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,28 @@
+import type { Preview } from '@storybook/vue3'
 import AppSvgSprite from '../app/components/app/AppIconSprite.vue'
 import '../app/assets/styles/app.css'
 
 export default {
+	parameters: {
+		backgrounds: {
+			default: 'Dark',
+			values: [
+				{
+					name: 'Light',
+					value: 'var(--color-zinc-50)',
+				},
+				{
+					name: 'Dark',
+					value: 'var(--color-zinc-900)',
+				},
+			],
+		},
+	},
 	decorators: [
 		(story) => ({
 			components: { story, AppSvgSprite },
 			template: '<div><AppSvgSprite /><story /></div>',
 		}),
 	],
-}
+	tags: ['autodocs'],
+} satisfies Preview
